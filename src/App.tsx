9f0913451d5c9474477457839e10801157f36d2b
MartinD-Aa <martindaa@hotmail.no>
2023-04-05 09:58:08 +0200

    Imported MonsterSlayer

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Health from "./components/Health";
+import Controls from "./components/Controls";
+import GameOver from "./components/GameOver";
+import Log from "./components/Log";
+import { useState } from "react";
 
 function App() {
+  const [gameSession, setGameSession] = useState<Boolean>(false);
+  const [playerHealth, setPlayerHealth] = useState<number>(100);
+  const [monsterHealth, setMonsterHealth] = useState<number>(100);
+  const [death, setDeath] = useState<Boolean>(false);
+  const [turn, setTurn] = useState<number>(0);
+
+  const SetHealthPlayer = (healthPlayer: number) => {
+	setPlayerHealth(healthPlayer);
+  };
+
+  const SetHealthMonster = (healthMonster: number) => {
+    setMonsterHealth(healthMonster);
+  };
+
+  const SetCurrentRound = (round: number) => {
+    setTurn(round);
+  };
+
+  const SetGameState = (gameState: Boolean) => {
+    setGameSession(gameState);
+  };
+
+  const SetDeathState = (deathState: Boolean) => {
+	setDeath(deathState);
+  }
+  if((playerHealth === 0 || monsterHealth === 0) && death === false ){
+	setDeath(true);
+	setGameSession(false);
+  }
+  console.log("Not dead " + turn + " " + playerHealth + " " + monsterHealth);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+		<div className="header">
+        	<h1>Monster Slayer</h1>
+    	</div>
+      { gameSession === false ?
+		<GameOver
+		player={playerHealth}
+		monster={monsterHealth}
+		state={gameSession}
+		death={death}
+		playerHealth={SetHealthPlayer}
+		monsterHealth={SetHealthMonster}
+		roundCount={SetCurrentRound}
+		gameState={SetGameState}
+		deathState = {SetDeathState}
+	  />
+	:
+	<div>
+  <Health player={playerHealth} monster={monsterHealth} />
+	<Controls
+	player={playerHealth}
+	monster={monsterHealth}
+	round={turn}
+	playerHealth={SetHealthPlayer}
+	monsterHealth={SetHealthMonster}
+	roundCount={SetCurrentRound}
+	gameState={SetGameState}
+	deathState = {SetDeathState}
+  />
+  <Log />
+  </div>
+  }
     </div>
   );
 }
---
+++ src/components/Controls.tsx
@@ -0,0 +1,89 @@
+function Controls(props: 
+    {player:number, 
+      monster:number,
+      round: number,
+      playerHealth: Function, 
+      monsterHealth: Function, 
+      roundCount: Function,
+      gameState: Function,
+      deathState: Function
+    }) {
+      var playerHP = props.player;
+      var monsterHP = props.monster;
+      var currentRound = props.round;
+      
+      
+  
+    function attackPlayer(){
+      const hit = randomTall(15, 8);
+      if(playerHP - hit < 0){
+        props.playerHealth(0);
+        
+      }else{
+        props.playerHealth(playerHP-= hit);
+      }
+      currentRound++;
+      props.roundCount(currentRound);
+    }
+  
+    function attackMonster(){
+      const hit = randomTall(12, 5);
+      if(monsterHP - hit < 0){
+        props.monsterHealth(0);
+        
+      } else{
+        props.monsterHealth(monsterHP -= hit);
+      }
+      attackPlayer();
+    }
+  
+    function specialAttack(){
+      const hit = randomTall(20, 15);
+      if(monsterHP - hit < 0){
+        props.monsterHealth(0);
+        
+      } else{
+        props.monsterHealth(monsterHP -= hit);
+      }
+      attackPlayer();
+    }
+  
+    function healPlayer(){
+      const heal = randomTall(20, 5);
+      const hit = randomTall(15, 8);
+      if(playerHP + (heal - hit) > 100){
+        props.playerHealth(100);
+      } else {
+        props.playerHealth(playerHP += (heal - hit));
+      }
+      currentRound++;
+      props.roundCount(currentRound);
+    }
+  
+    function flee(){
+      const num = randomTall(8, 2)
+      if(num < 5){
+        attackPlayer();
+      } else {
+        props.gameState(false);
+        props.deathState(true);
+      }
+    }
+  
+    function randomTall(min:number, max:number){
+      return Math.floor(Math.random() * (max-min)) + min;
+    }
+  
+  
+      return (
+      <section id="controls" className="container">
+      <button onClick={attackMonster}>ATTACK</button>
+      <button disabled={currentRound % 3 !== 0} onClick={specialAttack}>
+        SPECIAL ATTACK
+      </button>
+      <button onClick={healPlayer}>HEAL</button>
+      <button onClick={flee}>RUN</button>
+    </section>
+    );
+  }
+  export default Controls;
